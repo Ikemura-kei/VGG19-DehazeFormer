b86425a20b9f53339e544713cf999a2fe49dae50
IKEMURA-KEI <ikemurakei2001@gmail.com>
2022-10-10 19:15:55 +0800

    refactor

--- models/dehazeformer.py
+++ models/dehazeformer.py
@@ -25,26 +25,19 @@ def vgg19_conv1_2_feature_extractor():
     return ret
 
 
-def vgg19_image_preproc(pil_images):
+def vgg19_image_preproc(tensor_images):
     """
     preprocesses images in order to match the input requirement of VGG19 network
 
-    :param pil_images: a single PIL Image or a list of PIL Image's
-    :return: pytorch Tensor of shape (N,C,H,W), where N is 1 when pil_images is a single image and N is the length of the list otherwise
+    :param pil_images: a pytorch tensor of shape (N, C, H, W) or (C, H, W) for image(s)
+    :return: pytorch Tensor of the same shape as input
     """
     preprocessor = transforms.Compose(
         [
             transforms.Resize((224, 224)),
-            # transforms.ToTensor(),  # this also crops image into the range [0, 1]
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
     )
-
-    # if type(pil_images) is list:
-    #     tensor_list = [torch.unsqueeze(preprocessor(x), 0) for x in pil_images]
-    #     ret = torch.cat(tensor_list, 0)
-    # else:
-    #     ret = torch.unsqueeze(preprocessor(pil_images), 0)
     ret = preprocessor(pil_images)
 
     return ret
